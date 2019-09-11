1290747568136418f81d4083d5a8f7f2232b763e
Michael Ruppen <michael.ruppen@isolutions.ch>
2019-09-11 14:46:37 +0200

    Added DI project to this repository

---
+++ Abb.CqrsEs.DI/ICqrsEsBuilder.cs
@@ -0,0 +1,39 @@
+﻿using Abb.CqrsEs;
+using System;
+
+namespace Microsoft.Extensions.DependencyInjection
+{
+    public interface ICqrsEsBuilder
+    {
+        ICqrsEsOptionalBuilder Optional { get; }
+
+        ICqrsEsBuilder AddEventPublisher<T>() where T : IEventPublisher;
+
+        ICqrsEsBuilder AddEventPublisher(Type type);
+
+        ICqrsEsBuilder AddEventCache<T>() where T : IEventCache;
+
+        ICqrsEsBuilder AddEventCache(Type type);
+
+        ICqrsEsBuilder AddEventPersistence<T>() where T : IEventPersistence;
+
+        ICqrsEsBuilder AddEventPersistence(Type type);
+    }
+
+    public interface ICqrsEsOptionalBuilder
+    {
+        ICqrsEsBuilder RegisterHandlers();
+
+        ICqrsEsBuilder OverrideEventConverter<T>() where T : IEventConverter;
+
+        ICqrsEsBuilder OverrideEventConverter(Type type);
+
+        ICqrsEsBuilder EnableSnapshots<TStore>() where TStore : ISnapshotStore;
+
+        ICqrsEsBuilder EnableSnapshots<TStore, TStrategy>() where TStore : ISnapshotStore where TStrategy : ISnapshotStrategy;
+
+        ICqrsEsBuilder EnableSnapshots(Type snapshotStoreType);
+
+        ICqrsEsBuilder EnableSnapshots(Type snapshotStoreType, Type snapshotStrategyType);
+    }
+}

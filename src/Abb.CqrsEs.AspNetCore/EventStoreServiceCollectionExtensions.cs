a01463ffbb2ce3eea390282f1f5ac25f0149da99
Michael Ruppen <michael.ruppen@bit.admin.ch>
2020-05-26 15:50:01 +0200

    Updated formatting and removed warnings

--- src/Abb.CqrsEs.AspNetCore/EventStoreServiceCollectionExtensions.cs
+++ src/Abb.CqrsEs.AspNetCore/EventStoreServiceCollectionExtensions.cs
@@ -1,16 +1,13 @@
 ﻿using Abb.CqrsEs.Internal;
 using Microsoft.Extensions.DependencyInjection;
-using System;
 
 namespace Abb.CqrsEs.AspNetCore
 {
     public static class EventStoreServiceCollectionExtensions
     {
         public static IServiceCollection AddEventSourcing(this IServiceCollection services)
-        {
-            return services.AddSingleton<IAggregateFactory, DefaultAggregateFactory>()
+            => services.AddSingleton<IAggregateFactory, DefaultAggregateFactory>()
                 .AddSingleton<IAggregateRepository, AggregateRepository>()
                 .AddSingleton<CreateAggregateRootDelegate>(p => p.GetService);
-        }
     }
 }
--- src/Abb.CqrsEs/EventStreamException.cs
+++ src/Abb.CqrsEs/EventStreamException.cs
@@ -2,7 +2,10 @@
 
 namespace Abb.CqrsEs
 {
+#pragma warning disable S3925 // "ISerializable" should be implemented correctly
+
     public class EventStreamException : Exception
+#pragma warning restore S3925 // "ISerializable" should be implemented correctly
     {
         public EventStreamException(string message)
             : base(message)
